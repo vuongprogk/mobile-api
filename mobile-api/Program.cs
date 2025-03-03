cc38b1ec8d83131e136296a25e290d3cc1fc718a
vuongprogk <vuong753333@gmail.com>
2025-03-03 15:56:21 +0700

    fix: add runner to auto run migration

--- mobile-api/Program.cs
+++ mobile-api/Program.cs
@@ -49,4 +49,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.UseExceptionHandler(setting => { });
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<ApplicationDbContext>();
+try
+{
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 app.Run();
