ede83792a61305b9aa8343d00ba4f0c818f0cfe5
vuongprogk <vuong753333@gmail.com>
2025-05-09 06:50:42 +0700

    feat: upload img

---
+++ mobile-api/Migrations/20250508224827_Adding ImgURl.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mobile_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingImgURl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Tours",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Tickets",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Tickets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Tours");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Tickets");
+        }
+    }
+}
